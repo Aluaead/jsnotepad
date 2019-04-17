04dcab1b8d42f060d58e6173f2af738a52bc50ec
Aluaead <1076843408@qq.com>
2019-04-17 08:43:37 +0800

    2019-4-17

---
+++ dlg-about/jsnote.js
@@ -0,0 +1,40 @@
+/*exported $jsNote*/
+var $jsNote = (function(){
+
+  var DOM = ''
+            +'<div>'
+            +'<div class="container">'
+            +'<div class="top">'
+            +'<p class="title">关于“记事本”</p>'
+            +'<span class="close">×</span>'
+            +'</div>'
+            +'<div class="content">'
+            +'<h1 class="jsnote">JSNotepad</h1>'
+            +'<hr>'
+            +'<div class="text">'
+              +'<p>作者：李文轩</p>'
+              +'<p>QQ：1076843408</p>'
+              +'<p>仓库地址：<a href="https://github.com/Aluaead/jsnotepad" target="_blank">https://github.com/Aluaead/jsnotepad</a></p>'
+            +'</div>'
+            +'<input type="button" value="确定" class="del">'
+            +'</div>'
+            +'</div>'
+            +'</div>';
+  
+  var $jnp = $(DOM),
+      $close = $jnp.find('.close'),
+      $del = $jnp.find('.del');
+      
+
+  var show = function(){
+    $('body').append($jnp);
+    $jnp.find('.container').draggable({handle:$jnp.find('.top')});
+    $close.click(function(){
+      $jnp.remove();
+    });
+    $del.click(function(){
+      $jnp.remove();
+    });
+  };
+  return {show:show};
+}());
